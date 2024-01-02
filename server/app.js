1e6a1433dfbe6b28f62712bc375ad5fac9d2f79b
Maéva Lagrost <johndoe@exemple.com>
2024-01-02 16:50:04 +0100

    add CORS

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 mongoose
   .connect(
     "mongodb+srv://" +
@@ -9,7 +24,5 @@ mongoose
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
-
-const app = express();
 
 module.exports = app;
